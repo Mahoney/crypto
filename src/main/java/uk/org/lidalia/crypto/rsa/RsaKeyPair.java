6006ca541b7bec020e6ecaaad3ed63781b60a0c2
Robert Elliot <rob@lidalia.org.uk>
2014-06-03 22:44:36 +0100

    Added a static import

--- src/main/java/uk/org/lidalia/crypto/rsa/RsaKeyPair.java
+++ src/main/java/uk/org/lidalia/crypto/rsa/RsaKeyPair.java
@@ -5,17 +5,18 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateCrtKey;
 
+import static uk.org.lidalia.crypto.rsa.RsaKeyUtils.RSA_ALGORITHM_NAME;
 import static uk.org.lidalia.crypto.rsa.RsaKeyUtils.requiredAlgorithmNotPresentException;
 
 public class RsaKeyPair {
 
-    public static RsaKeyPair generate() {
+    public static RsaKeyPair generate() throws IllegalStateException {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RsaKeyUtils.RSA_ALGORITHM_NAME);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA_ALGORITHM_NAME);
             keyPairGenerator.initialize(1024);
             return from(keyPairGenerator.generateKeyPair());
         } catch (NoSuchAlgorithmException e) {
-            throw requiredAlgorithmNotPresentException(e, RsaKeyUtils.RSA_ALGORITHM_NAME);
+            throw requiredAlgorithmNotPresentException(e, RSA_ALGORITHM_NAME);
         }
     }
 
