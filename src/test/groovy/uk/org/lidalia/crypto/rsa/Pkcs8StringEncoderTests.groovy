6bbcd7f4e53c9a99626d065c47f3e98c06cb40c2
Robert Elliot <rob@lidalia.org.uk>
2017-02-16 21:22:24 +0000

    Test byte encoders in isolation

--- src/test/groovy/uk/org/lidalia/crypto/rsa/Pkcs8StringEncoderTests.groovy
+++ src/test/groovy/uk/org/lidalia/crypto/rsa/Pkcs8StringEncoderTests.groovy
@@ -2,9 +2,9 @@ package uk.org.lidalia.crypto.rsa
 
 import uk.org.lidalia.encoding.EncoderTests
 
-class Pkcs8EncoderTests extends EncoderTests<RsaPrivateKey, String, Pkcs8String> {
+class Pkcs8StringEncoderTests extends EncoderTests<RsaPrivateKey, String, Pkcs8String> {
 
-    Pkcs8Encoder encoder = Pkcs8Encoder.pkcs8
+    Pkcs8StringEncoder encoder = Pkcs8StringEncoder.pkcs8String
     RsaPrivateKey instance1 = RsaPrivateKey.generate()
     RsaPrivateKey instance2 = RsaPrivateKey.generate()
 }
--- src/test/groovy/uk/org/lidalia/crypto/rsa/Rfc2453PublicKeyStringEncoderTests.groovy
+++ src/test/groovy/uk/org/lidalia/crypto/rsa/Rfc2453PublicKeyStringEncoderTests.groovy
@@ -2,9 +2,9 @@ package uk.org.lidalia.crypto.rsa
 
 import uk.org.lidalia.encoding.EncoderTests
 
-class Rfc2453PublicKeyEncoderTests extends EncoderTests<RsaPublicKey, String, Rfc2453PublicKeyString> {
+class Rfc2453PublicKeyStringEncoderTests extends EncoderTests<RsaPublicKey, String, Rfc2453PublicKeyString> {
 
-    Rfc2453PublicKeyEncoder encoder = Rfc2453PublicKeyEncoder.rfc2453PublicKey
+    Rfc2453PublicKeyStringEncoder encoder = Rfc2453PublicKeyStringEncoder.rfc2453PublicKeyString
     RsaPublicKey instance1 = RsaPrivateKey.generate().publicKey()
     RsaPublicKey instance2 = RsaPrivateKey.generate().publicKey()
 }
