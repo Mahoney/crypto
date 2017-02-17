f1ae8bac0b5060f427ee2298acf3d0d5246c731d
Robert Elliot <rob@lidalia.org.uk>
2017-02-17 09:08:06 +0000

    Make class that should be public, public

--- src/main/java/uk/org/lidalia/crypto/rsa/Pkcs1.java
+++ src/main/java/uk/org/lidalia/crypto/rsa/Pkcs1.java
@@ -4,7 +4,7 @@ import uk.org.lidalia.encoding.Bytes;
 import uk.org.lidalia.encoding.CachedEncodedBase;
 import uk.org.lidalia.encoding.InvalidEncoding;
 
-class Pkcs1 extends CachedEncodedBase<RsaPrivateKey, Bytes> {
+public class Pkcs1 extends CachedEncodedBase<RsaPrivateKey, Bytes> {
 
     Pkcs1(Bytes raw, RsaPrivateKey decoded) throws InvalidEncoding {
         super(raw, decoded);
