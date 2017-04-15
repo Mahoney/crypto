d47329bb2b4eb4467b87300a1080556e7f1be13e
Robert Elliot <rob@lidalia.org.uk>
2017-04-15 14:39:29 +0100

    Add some javadoc to explain the types Encoder and Encoded

--- src/main/java/uk/org/lidalia/encoding/Encoded.java
+++ src/main/java/uk/org/lidalia/encoding/Encoded.java
@@ -1,9 +1,24 @@
 package uk.org.lidalia.encoding;
 
+/**
+ * Represents an encoded form of some value - for instance, a byte[] encoded as a hex string.
+ *
+ * Should be validated as part of construction - that is, calling either method on this interface
+ * should never result in an Exception.
+ *
+ * @param <Decoded> the type of the actual value that has been encoded (in the case of a hex string, {@link Bytes})
+ * @param <Raw> the type of the encoding (in the case of a hex string, a {@link String})
+ */
 public interface Encoded<Decoded, Raw> {
 
+    /**
+     * @return the original unencoded value
+     */
     Decoded decode();
 
+    /**
+     * @return the raw encoded form
+     */
     Raw raw();
 
 }
--- src/main/java/uk/org/lidalia/encoding/Encoder.java
+++ src/main/java/uk/org/lidalia/encoding/Encoder.java
@@ -1,9 +1,35 @@
 package uk.org.lidalia.encoding;
 
+/**
+ * Type that allows encoding a value to some encoded form (e.g.
+ * encoding some {@link Bytes} as {@link uk.org.lidalia.encoding.hex.Hex},
+ * or constructing a validated encoded form from a raw encoded form
+ * (e.g. a {@link uk.org.lidalia.encoding.hex.Hex} from a {@link String}).
+ *
+ * @param <Decoded> the type of the actual value that has been encoded
+ *                 (in the case of a hex string, {@link Bytes})
+ * @param <RawEncoded> the type of the encoding (in the case of a hex string,
+ *                    a {@link String})
+ * @param <E> the {@link Encoded} type
+ */
 public interface Encoder<Decoded, RawEncoded, E extends Encoded<Decoded, RawEncoded>> {
 
+    /**
+     * Validates a raw encoded value is a valid encoded form of the unencoded
+     * type, and returns it wrapped in a type safe value that permits decoding
+     *
+     * @param encoded the raw encoded value (e.g. a hex string)
+     * @return a validated and type safe wrapper around the encoded param
+     * @throws InvalidEncoding if the encoded param is not a valid encoded form
+     */
     E of(RawEncoded encoded) throws InvalidEncoding;
 
+    /**
+     * Encodes a value in some more general form
+     *
+     * @param decoded the original unencoded value
+     * @return type safe encoded form of the value
+     */
     E encode(Decoded decoded);
 
 }
