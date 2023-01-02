db6af6f9d3f5d4d759fd4c81888493d88546ede9
Robert Elliot <rob@lidalia.org.uk>
2023-01-02 13:08:51 +0000

    Add equals, hashcode and toString to EncryptedBytes

--- src/main/java/uk/org/lidalia/crypto/EncryptedBytes.java
+++ src/main/java/uk/org/lidalia/crypto/EncryptedBytes.java
@@ -2,6 +2,11 @@ package uk.org.lidalia.crypto;
 
 import uk.org.lidalia.lang.Bytes;
 
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+import static uk.org.lidalia.encoding.base64.Base64Encoder.base64;
+
 public final class EncryptedBytes {
 
     private final Bytes bytes;
@@ -15,10 +20,28 @@ public final class EncryptedBytes {
     }
 
     private EncryptedBytes(Bytes bytes) {
-        this.bytes = bytes;
+        this.bytes = requireNonNull(bytes);
     }
 
     public Bytes bytes() {
         return bytes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EncryptedBytes that = (EncryptedBytes) o;
+        return bytes.equals(that.bytes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bytes);
+    }
+
+    @Override
+    public String toString() {
+        return base64.encode(bytes).raw();
     }
 }
