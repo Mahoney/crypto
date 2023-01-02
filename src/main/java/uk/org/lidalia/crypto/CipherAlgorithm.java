68342f829a27a560caa6767beb3e63b0e36b8650
Robert Elliot <rob@lidalia.org.uk>
2023-01-02 13:08:19 +0000

    Document what a CipherX is

--- src/main/java/uk/org/lidalia/crypto/CipherAlgorithm.java
+++ src/main/java/uk/org/lidalia/crypto/CipherAlgorithm.java
@@ -1,5 +1,14 @@
 package uk.org.lidalia.crypto;
 
+/**
+ * A CipherAlgorithm is an algorithm used for encryption and decryption.
+ * An example would be RSA, or a symmetric encryption algorithm.
+ * An example of a KeyAlgorithm that is not a CipherAlgorithm would be DSA - it
+ * only signs and verifies.
+ *
+ * @param <Encrypt> The type of the key used for encrypting in this algorithm
+ * @param <Decrypt> The type of the key used for decrypting in this algorithm
+ */
 public interface CipherAlgorithm<
         Encrypt extends EncryptKey<Encrypt, Decrypt>,
         Decrypt extends DecryptKey<Encrypt, Decrypt>
--- src/main/java/uk/org/lidalia/crypto/CipherKey.java
+++ src/main/java/uk/org/lidalia/crypto/CipherKey.java
@@ -1,5 +1,11 @@
 package uk.org.lidalia.crypto;
 
+/**
+ * A CipherKey is a key used for encryption or decryption.
+ *
+ * @param <Encrypt> The type of the key used for encrypting in this algorithm
+ * @param <Decrypt> The type of the key used for decrypting in this algorithm
+ */
 public interface CipherKey<
         Encrypt extends EncryptKey<Encrypt, Decrypt>,
         Decrypt extends DecryptKey<Encrypt, Decrypt>
